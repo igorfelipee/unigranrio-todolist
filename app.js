c657fd08f6df59f7c56e982e47ce85ec1b3d19e1
Igor Felipe de Jesus <igor.jesus@b2wdigital.com>
2019-03-19 14:03:23 -0300

    cors

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const cors = require('cors')
 var express = require('express')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
@@ -5,6 +6,7 @@ var logger = require('morgan')
 var indexRouter = require('./routes/index')
 
 var app = express()
+app.use(cors())
 
 const mongo = require('./config/mongo')
 
